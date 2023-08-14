f1c0c9dfc1abe69294fe364bde46c8cb1ed70366
pearlKinn <jinju000202@naver.com>
2023-08-14 13:02:54 +0900

    사이드 이펙트 공부

--- src/pages/Practice/parts/FilterableList.jsx
+++ src/pages/Practice/parts/FilterableList.jsx
@@ -1,15 +1,23 @@
-import styles from '@/styles/FilterableList.module.css'
+import styles from '@/styles/FilterableList.module.css';
+import SideEffect from '@/components/SideEffect';
 
 function FilterableList() {
   return (
     <>
+      <SideEffect />
       <form>
-        <label htmlFor="todo"></label>
-        <input type="text" id="todo" placeholder="휴일에 할 일" className={styles.primary}/>
+        <div className={styles.accent}>
+          <label htmlFor="todo"></label>
+          <input
+            id="todo"
+            type="text"
+            placeholder="휴일에 할 일"
+          />
+        </div>
         <button type="submit">추가</button>
       </form>
     </>
-  );
+  )
 }
 
-export default FilterableList;
+export default FilterableList
