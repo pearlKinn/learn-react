bb7a35d9374bd9080f53818da44987fbc7d664f4
pearlKinn <jinju000202@naver.com>
2023-08-16 11:40:25 +0900

    form field 업데이트

--- src/layout/HeaderBar.jsx
+++ src/layout/HeaderBar.jsx
@@ -1,9 +1,9 @@
-import Heading from "../components/Heading";
+import Heading from "@/components/Heading";
 import styles from '@/styles/HeaderBar.module.css'
 
 function HeaderBar() {
   return (
-    <header className={styles.container}>
+    <header className="p-5 bg-indigo-950 text-white">
       <Heading  />
     </header>
   );
