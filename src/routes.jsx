94ebbb6a91fc0fee8863fd14ceb3f8d275055554
pearlKinn <jinju000202@naver.com>
2023-08-21 16:37:08 +0900

    동적 라우팅, getSlug 함수 생성, useProductList hook 생성, 상품편집 페이지 폼 작성

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,20 +1,20 @@
-import { createBrowserRouter } from 'react-router-dom';
+import { Route, createBrowserRouter, createRoutesFromElements } from 'react-router-dom';
 import RootLayout from './layout/RootLayout';
 import Home from './pages/Home';
 import Products from './pages/Products';
+import ProductEdit from './pages/ProductEdit';
 import Contact from './pages/Contact';
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <RootLayout />,
-    // 중첩된 라우트
-    children: [
-      { index: true, element: <Home /> },
-      { path: 'products', element: <Products /> },
-      { path: 'contact', element: <Contact /> },
-    ],
-  },
-]);
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<RootLayout />}>
+      <Route index element={<Home />} />
+      <Route path="products" element={<Products />} />
+      <Route path="product/edit/:productId" element={<ProductEdit />} />
+      <Route path="contact" element={<Contact />} />
+    </Route>
+  )
+)
 
 export default router;
