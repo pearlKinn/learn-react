0a5a772b09797b151187006792cb0519f3151cad
pearlKinn <jinju000202@naver.com>
2023-08-25 13:14:31 +0900

    인증에 따른 라우팅 접근 시도 중 발생한 경고 해결 - 렌더링에 문제를 일으키는 상태 변경 요청 코드를 useEffect 내부로 옮김 (사이드 이펙트)

--- src/components/ProtectRoute.jsx
+++ src/components/ProtectRoute.jsx
@@ -1,29 +1,35 @@
-import { useAuth } from "@/contexts/Auth"
-import { Navigate } from "react-router-dom"
-import {element} from 'prop-types'
-import toast from "react-hot-toast"
+import { useAuth } from "@/contexts/Auth";
+import { Navigate } from "react-router-dom";
+import { element } from "prop-types";
+import toast from "react-hot-toast";
+import { useEffect } from "react";
 
+function ProtectRoute({ children }) {
+  const { isAuth } = useAuth();
 
-function ProtectRoute({children}) {
-  const {isAuth} = useAuth()
-  if(!isAuth) {
-    toast('로그인 된 사용자만 이용 가능한 페이지입니다.', {
-      icon: '⚠️',
-      ariaProps: {
-        role:'alert',
-        'aria-live':'polite'
-      }
-    })
-    return <Navigate to='/signin'/>
+  useEffect(() => {
+    // if (!isAuth) {
+      toast("로그인 된 사용자만 이용 가능한 페이지입니다.", {
+        icon: "⚠️",
+        ariaProps: {
+          role: "alert",
+          "aria-live": "polite",
+        },
+      });
+    // }
+
+    return () => {};
+  }, [isAuth]);
+
+  if (!isAuth) {
+    return <Navigate to="/signin" />;
   }
 
-  return (
-    children
-  )
+  return children;
 }
 
 ProtectRoute.propTypes = {
-  children: element
-}
+  children: element,
+};
 
-export default ProtectRoute
+export default ProtectRoute;
